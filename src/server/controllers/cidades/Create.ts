68b40d545b07494a9823e33d2fe37c8c1f2b3bd8
Lucas Menezes <livedolux@gmail.com>
2024-08-24 19:22:09 -0300

    feat: Add yup validation to create cidades controller

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,10 +1,28 @@
 import { Response, Request } from "express";
+import * as yup from "yup";
+import { ObjectSchema, number, string, object } from "yup";
+import { StatusCodes } from "http-status-codes";
 
 interface Icidade {
   nome: string;
 }
 
-export const create = (req: Request<{}, {}, Icidade>, res: Response): Response => {
-  const { nome } = req.body;
-  return res.json({ nome: nome });
+let cidadeSchema: ObjectSchema<Icidade> = object({
+  nome: string().required(),
+});
+
+export const create = async (
+  req: Request<{}, {}, Icidade>,
+  res: Response
+): Promise<Response> => {
+  try {
+    const cidade = await cidadeSchema.validate(req.body);
+    return res.json({ cidade: cidade });
+  } catch (error) {
+    const yupError = error as yup.ValidationError;
+
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      error: yupError.message,
+    });
+  }
 };
